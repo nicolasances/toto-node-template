f6daf8b0b2ec4036c85d2693372c41a43b77d703
nicolasances <nicolas.matteazzi@gmail.com>
2026-01-12 08:53:02 +0100

    Added Artifact Registry to Terraform

--- gcp/terraform/toto-ms-xxx.tf
+++ gcp/terraform/toto-ms-xxx.tf
@@ -4,6 +4,29 @@
 # ###############################################################
 # ###############################################################
 # ---------------------------------------------------------------
+# 0. Artifact Registry
+# ---------------------------------------------------------------
+resource "google_artifact_registry_repository" "toto-ms-xxx-artifact-repo" {
+    location = "europe-west1"
+    repository_id = "toto-ms-xxx"
+    description = "Artifact Registry for toto-ms-xxx"
+    format = "DOCKER"
+    
+    docker_config {
+        immutable_tags = true
+    }
+
+    cleanup_policy_dry_run = false
+    cleanup_policies {
+        id     = "keep-minimum-versions"
+        action = "KEEP"
+        most_recent_versions {
+            keep_count            = 1
+        }
+    }
+}
+
+# ---------------------------------------------------------------
 # 1. Service Account 
 # ---------------------------------------------------------------
 # 1.1. Service Account 
