ef841dc62017b0d097e45cf5d42979fbf31cfae7
nicolasances <nicolas.matteazzi@gmail.com>
2026-04-25 15:34:12 +0200

    Updated to totoms 2.3.2

--- src/dlg/ExampleDelegate.ts
+++ src/dlg/ExampleDelegate.ts
@@ -1,10 +1,22 @@
 import { Request } from "express";
 import { TotoDelegate, UserContext } from "totoms";
 
-export class SayHello extends TotoDelegate {
+export class SayHello extends TotoDelegate<SayHelloRequest, SayHelloResponse> {
 
-    async do(req: Request, userContext?: UserContext): Promise<any> {
+    async do(req: Request, userContext?: UserContext): Promise<SayHelloResponse> {
         return { message: "Hello World!" };
     }
 
+    parseRequest(req: Request): SayHelloRequest {
+        return {}
+    }
+
+}
+
+interface SayHelloRequest {
+
+}
+
+interface SayHelloResponse {
+    message: string;
 }
